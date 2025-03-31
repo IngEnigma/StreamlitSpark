2a17e5b77f58444f54be982cd0a9235935004f94
IngEnigma <zs22019956@estudiantes.uv.mx>
2025-03-31 08:01:37 +0000

    save spark results

--- crime.py
+++ crime.py
@@ -1,13 +1,11 @@
 from pyspark.sql import SparkSession
-import os
+import json
 
 if __name__ == "__main__":
     spark = SparkSession \
         .builder \
-        .appName("CrimeData") \
+        .appName("crime_data") \
         .getOrCreate()
-
-    os.makedirs("results", exist_ok=True)  
 
     print("read dataset.csv ... ")
     
@@ -15,36 +13,58 @@ if __name__ == "__main__":
     
     df_crimes = spark.read.csv(path_crimes, header=True, inferSchema=True)
 
-    df_crimes = df_crimes.withColumnRenamed("Vict Age", "victim_age") \
+    df_crimes = df_crimes.withColumnRenamed("DR_NO", "dr_no") \
+                         .withColumnRenamed("Date Rptd", "report_date") \
+                         .withColumnRenamed("DATE OCC", "date_occ") \
+                         .withColumnRenamed("TIME OCC", "time_occ") \
+                         .withColumnRenamed("AREA", "area") \
+                         .withColumnRenamed("AREA NAME", "area_name") \
+                         .withColumnRenamed("Rpt Dist No", "rpt_dist_no") \
+                         .withColumnRenamed("Part 1-2", "part_1_2") \
+                         .withColumnRenamed("Crm Cd", "crm_cd") \
+                         .withColumnRenamed("Crm Cd Desc", "crm_cd_desc") \
+                         .withColumnRenamed("Mocodes", "mocodes") \
+                         .withColumnRenamed("Vict Age", "victim_age") \
                          .withColumnRenamed("Vict Sex", "victim_sex") \
                          .withColumnRenamed("Vict Descent", "victim_descent") \
-                         .withColumnRenamed("Date Rptd", "report_date") \
-                         .withColumnRenamed("Crm Cd Desc", "crm_cd_desc")
+                         .withColumnRenamed("Premis Cd", "premis_cd") \
+                         .withColumnRenamed("Premis Desc", "premis_desc") \
+                         .withColumnRenamed("Weapon Used Cd", "weapon_used_cd") \
+                         .withColumnRenamed("Weapon Desc", "weapon_desc") \
+                         .withColumnRenamed("Status", "status") \
+                         .withColumnRenamed("Status Desc", "status_desc")
 
     df_crimes.createOrReplaceTempView("crimes")
 
-    query = """SELECT DR_NO, report_date, victim_age, victim_sex, crm_cd_desc
+    query = 'DESCRIBE crimes'
+    spark.sql(query).show(20)
+
+    query = """SELECT dr_no, report_date, victim_age, victim_sex, crm_cd_desc 
                FROM crimes WHERE victim_sex = 'M' 
                ORDER BY report_date"""
     df_male_crimes = spark.sql(query)
-    df_male_crimes.write.json("results/male_crimes", mode="overwrite")
+    df_male_crimes.show(20)
 
-    query = '''SELECT DR_NO, report_date, victim_age, victim_sex, crm_cd_desc 
+    query = """SELECT dr_no, report_date, victim_age, victim_sex, crm_cd_desc 
                FROM crimes WHERE report_date BETWEEN '2019-01-01' AND '2020-12-31' 
-               ORDER BY report_date'''
+               ORDER BY report_date"""
     df_crimes_2019_2020 = spark.sql(query)
-    df_crimes_2019_2020.write.json("results/crimes_2019_2020", mode="overwrite")
+    df_crimes_2019_2020.show(20)
 
-    query = '''SELECT AREA, COUNT(AREA) as crime_count 
+    results = df_crimes_2019_2020.toJSON().collect()
+    with open('results/crimes_2019_2020.json', 'w') as file:
+        json.dump(results, file)
+
+    query = """SELECT area, COUNT(area) as crime_count 
                FROM crimes 
-               GROUP BY AREA ORDER BY crime_count DESC'''
+               GROUP BY area ORDER BY crime_count DESC"""
     df_crimes_by_area = spark.sql(query)
-    df_crimes_by_area.write.json("results/crimes_by_area", mode="overwrite")
+    df_crimes_by_area.show()
 
-    query = '''SELECT DR_NO, report_date, victim_age, victim_sex, crm_cd_desc
+    query = """SELECT dr_no, report_date, victim_age, victim_sex, crm_cd_desc 
                FROM crimes WHERE victim_age BETWEEN 18 AND 30 
-               ORDER BY victim_age'''
+               ORDER BY victim_age"""
     df_young_adults_crimes = spark.sql(query)
-    df_young_adults_crimes.write.json("results/young_adults_crimes", mode="overwrite")
+    df_young_adults_crimes.show(20)
 
     spark.stop()
